246fffdccd00a2fb8acfbc4457300ea8c1f0a700
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-01-31 12:20:38 -0800

    Update Bingo-card.js

--- Bingo-card.js
+++ Bingo-card.js
@@ -20,3 +20,24 @@ Within the columns, the order of the numbers is random.
 
 
 // Solution
+
+function genRange(arr, letter, start, end, count) {
+  let taken = [];
+  for (let i = 0; i < count;) {
+    r = Math.floor(Math.random() * (end - start + 1) + start);
+    if (taken.indexOf(r) == -1) {
+      taken.push(r);
+      arr.push(letter + r);
+      i++;
+    }
+  }
+}
+function getCard() {
+  let arr = [];
+  genRange(arr, 'B', 1, 15, 5);
+  genRange(arr, 'I', 16, 30, 5);
+  genRange(arr, 'N', 31, 45, 4);
+  genRange(arr, 'G', 46, 60, 5);
+  genRange(arr, 'O', 61, 75, 5);
+  return arr;
+}
